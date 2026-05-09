fbf469abf34060c7775fd7833165801949ea74c0
humanreliefmission-160 <media@humanreliefmission.com>
2026-05-09 17:55:57 +0100

    Added Sanity

---
+++ app/studio/[[...tool]]/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import "@/app/globals.css";
+
+export const metadata: Metadata = {
+  title: "Human Relief Mission",
+  description: "Helping Humanity Through Welfare",
+};
+
+export default function StudioLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik+Dirt&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet" />
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
