c47608467a11d6536d5dd212aee01c4e26971d6b
humanreliefmission-160 <media@humanreliefmission.com>
2026-08-29 12:29:29 +0100

    Login page for donations admin portal complete

--- app/[locale]/(admin)/donations/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getDonations } from "./actions";
-import DonationsClient from "./DonationsClient";
-
-export default async function DonationsPage() {
-  const donations = await getDonations();
-  return <DonationsClient donations={donations} />;
-}
